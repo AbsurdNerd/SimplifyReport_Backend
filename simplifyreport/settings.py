326671cb13367af9a966347f32d1de0ff71a7d77
todi-2000 <manshitodi@gmail.com>
2020-11-13 01:55:49 +0530

    Heroku

--- simplifyreport/settings.py
+++ simplifyreport/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Heroku
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
